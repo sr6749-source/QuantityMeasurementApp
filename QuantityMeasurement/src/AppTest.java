f5c20d5cfedab75e66f7513c0520609bbffb9bd0
sr6749-source <sr6749@srmist.edu.in>
2026-04-24 10:38:57 +0530

    UC9 completed by 11349

--- QuantityMeasurement/src/AppTest.java
+++ QuantityMeasurement/src/AppTest.java
@@ -6,42 +6,22 @@ class AppTest {
     private static final double EPS = 1e-3;
 
     @Test
-    void testEquality() {
+    void testLengthEquality() {
         assertTrue(new QuantityLength(1.0, LengthUnit.FEET)
                 .equals(new QuantityLength(12.0, LengthUnit.INCH)));
     }
 
     @Test
-    void testConversion() {
-        assertEquals(12.0,
-                new QuantityLength(1.0, LengthUnit.FEET)
-                        .convertTo(LengthUnit.INCH).getValue(),
-                EPS);
+    void testWeightEquality() {
+        assertTrue(new QuantityWeight(1.0, WeightUnit.KILOGRAM)
+                .equals(new QuantityWeight(1000.0, WeightUnit.GRAM)));
     }
 
     @Test
-    void testAddition() {
-        assertEquals(2.0,
-                new QuantityLength(1.0, LengthUnit.FEET)
-                        .add(new QuantityLength(12.0, LengthUnit.INCH))
-                        .getValue(),
+    void testWeightConversion() {
+        assertEquals(2.20462,
+                new QuantityWeight(1.0, WeightUnit.KILOGRAM)
+                        .convertTo(WeightUnit.POUND).getValue(),
                 EPS);
-    }
-
-    @Test
-    void testAddition_TargetUnit() {
-        assertEquals(24.0,
-                QuantityLength.add(
-                        new QuantityLength(1.0, LengthUnit.FEET),
-                        new QuantityLength(12.0, LengthUnit.INCH),
-                        LengthUnit.INCH).getValue(),
-                EPS);
-    }
-
-    @Test
-    void testInvalidUnit() {
-        assertThrows(IllegalArgumentException.class, () -> {
-            new QuantityLength(1.0, null);
-        });
     }
 }
