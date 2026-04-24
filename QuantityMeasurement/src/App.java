3919ede47bfb9226b6135745e1d83a7dbc33e397
sr6749-source <sr6749@srmist.edu.in>
2026-04-24 09:39:01 +0530

    UC3 completed by 11349

--- QuantityMeasurement/src/App.java
+++ QuantityMeasurement/src/App.java
@@ -1,6 +1,15 @@
 public class App {
-    static void main(String[] args) {
-        System.out.println("Quantity Measurement App");
-    }
 
+    public static void main(String[] args) {
+
+        QuantityLength q1 = new QuantityLength(1.0, LengthUnit.FEET);
+        QuantityLength q2 = new QuantityLength(12.0, LengthUnit.INCH);
+
+        System.out.println("Equal (" + q1.equals(q2) + ")");
+
+        QuantityLength q3 = new QuantityLength(1.0, LengthUnit.INCH);
+        QuantityLength q4 = new QuantityLength(1.0, LengthUnit.INCH);
+
+        System.out.println("Equal (" + q3.equals(q4) + ")");
+    }
 }
