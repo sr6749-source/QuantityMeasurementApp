41e7142192b5890e3ef1c0fefaeed36edfa4f0a7
sr6749-source <sr6749@srmist.edu.in>
2026-04-24 09:52:15 +0530

    Update App.java

--- QuantityMeasurement/src/App.java
+++ QuantityMeasurement/src/App.java
@@ -1,30 +1,15 @@
 public class App {
 
-    public static void demonstrateLengthConversion(double value, LengthUnit from, LengthUnit to) {
-        double result = QuantityLength.convert(value, from, to);
-        System.out.println("Converted: " + result);
-    }
-
-    public static void demonstrateLengthConversion(QuantityLength length, LengthUnit to) {
-        QuantityLength converted = length.convertTo(to);
-        System.out.println("Converted: " + converted);
-    }
-
-    public static void demonstrateLengthEquality(QuantityLength q1, QuantityLength q2) {
-        System.out.println("Equal: " + q1.equals(q2));
-    }
-
     public static void main(String[] args) {
 
-        demonstrateLengthConversion(1.0, LengthUnit.FEET, LengthUnit.INCH); // 12
-        demonstrateLengthConversion(3.0, LengthUnit.YARDS, LengthUnit.FEET); // 9
+        QuantityLength q1 = new QuantityLength(1.0, LengthUnit.FEET);
+        QuantityLength q2 = new QuantityLength(12.0, LengthUnit.INCH);
 
-        QuantityLength q = new QuantityLength(36.0, LengthUnit.INCH);
-        demonstrateLengthConversion(q, LengthUnit.YARDS); // 1
+        System.out.println("Equal (" + q1.equals(q2) + ")");
 
-        demonstrateLengthEquality(
-                new QuantityLength(1.0, LengthUnit.FEET),
-                new QuantityLength(12.0, LengthUnit.INCH)
-        );
+        QuantityLength q3 = new QuantityLength(1.0, LengthUnit.INCH);
+        QuantityLength q4 = new QuantityLength(1.0, LengthUnit.INCH);
+
+        System.out.println("Equal (" + q3.equals(q4) + ")");
     }
 }
